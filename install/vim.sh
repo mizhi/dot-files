e01a6335935891aadc615a922a62d7cc88cc6337
Mitch Peabody <mpeabody@panoramaed.com>
2017-12-30 17:12:31 -0500

    Fixup vim installation

--- install/vim.sh
+++ install/vim.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-if [! -d ~/.vim ]; then
+if [ ! -d ~/.vim ]; then
     mkdir ~/.vim
 fi
 
-if [! -d ~/.vim/bundle/Vundle.vim ]; then
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
@@ -13,5 +13,10 @@ ln -sf `pwd`/vimrc ~/.vimrc
 vim +PluginInstall +qall
 
 pushd $HOME/.vim/bundle/YouCompleteMe && ./install.py --clang-completer && popd 
+
+
+if [ ! -d ~/.vim/colors ]; then
+  mkdir -p ~/.vim/colors
+fi
 
 curl https://raw.githubusercontent.com/jnurmine/Zenburn/master/colors/zenburn.vim -o $HOME/.vim/colors/zenburn.vim
